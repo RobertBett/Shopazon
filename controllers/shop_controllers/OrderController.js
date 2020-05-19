6782a57f857679d82a22d1d67e59bc8c7a099357
RobertBett <robertbett6@icloud.com>
2020-05-19 00:08:57 -0400

    complete app using SQL

--- controllers/shop_controllers/OrderController.js
+++ controllers/shop_controllers/OrderController.js
@@ -2,13 +2,21 @@ const chalk = require("chalk");
 const CartItem = require('../../models/CartItem');
 
 exports.getOrders = (req,res, next) =>{
-    res.render('shop/orders', {
+    req.user.getOrders({ include:['products']})
+    .then((orders) => {
+        res.render('shop/orders', {
         pageTitle: 'orders',
+        orders,
         path:'/orders',
         formsCSS: true,
         productCSS: true,
         activeAddProduct: true
     });
+    })
+    .catch((err) => {
+        console.error(err);
+    });
+
 };
 
 exports.getCheckout = (req,res, next) =>{
@@ -21,17 +29,35 @@ exports.getCheckout = (req,res, next) =>{
     });
 };
 
-exports.postOrder = (req, res, next)=>{
-    CartItem.findAll({})
-    req.user.getCart({ where:{ userId: req.user.id}})
+exports.postOrder = (req, res, next) =>{
+    let fetchedCart;
+    req.user.getCart({ where: { userId: req.user.id}})
     .then((cart) => {
-        return cart.getProducts({ where:{ id:productId }})
+        fetchedCart = cart;
+        return cart.getProducts()
+    })
+    .then((products) => {
+        req.user.createOrder()
+        .then((order) => {
+            return order.addProducts(
+                products.map( product =>{
+                    product.orderItem = { 
+                        quantity: product.cartItem.quantity,
+                        orderTotal: product.price * product.cartItem.quantity
+                    };
+                    return product;
+            }));
+        }).catch((err) => {
+            console.error(err);
+        });
     })
     .then(() => {
-        
+        return fetchedCart.setProducts(null)
+    })
+    .then((result) => {
+        res.redirect('/orders')
     })
     .catch((err) => {
         console.error(err);
-        
     });
 }
